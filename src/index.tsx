c0496445722fb231be309b904e04dc79ef43e3a8
Andrii Prykhodko <andriiprykhodko@Andriis-Air.home>
2023-11-16 14:10:40 +0000

    index

--- src/index.tsx
+++ src/index.tsx
@@ -1,24 +1,13 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import { createStore, applyMiddleware } from 'redux'
-import rootReducer from './reducers'
 import { Provider } from 'react-redux'
-import thunk from 'redux-thunk';
 // import logger from 'redux-logger'
-import { composeWithDevTools } from 'redux-devtools-extension'
 import type {} from 'redux-thunk/extend-redux'
 import App from './App'
+import store from './store'
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
-)
-
-const store = createStore(
-  rootReducer,
-  composeWithDevTools(
-    applyMiddleware(thunk)
-    )
 )
 
 root.render(
