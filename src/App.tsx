92574385f3d64643a987c8d7d98bc419c3e71d76
Andrii Prykhodko <andriiprykhodko@Andriis-Air.home>
2023-11-16 14:11:32 +0000

    app

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,20 @@ import { useEffect } from 'react'
 import GraphView from "./components/right-side/GraphView"
 import LeftView from "./components/left-side/LeftView"
 import { useSelector, useDispatch } from 'react-redux'
-import { setGraphData } from './actions/graph.action'
+import { setGraphData } from "./reducers/graph.reducer"
 
 const App = () => {
   const graphData = useSelector((store: Store) => store.graphData)
 
   const dispatch = useDispatch()
   
+  useEffect(() => {
+    localStorage.setItem(
+      'react-graph-app-data', 
+    JSON.stringify(graphData)
+    )
+  }, [graphData])
+
   useEffect(() => {
     const localStoreGraphStr = localStorage.getItem('react-graph-app-data')
     if (typeof localStoreGraphStr === 'string') 
@@ -20,13 +27,6 @@ const App = () => {
     }
     
   }, [dispatch])
-
-  useEffect(() => {
-    localStorage.setItem(
-      'react-graph-app-data', 
-    JSON.stringify(graphData)
-    )
-  }, [graphData])
 
   return (
     <div className="container"> 
