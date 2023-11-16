ce9e4b9b0ea305f5a42175470fad9ff5db9d9277
Andrii Prykhodko <andriiprykhodko@Andriis-Air.home>
2023-11-16 17:20:55 +0000

    cleanup comments

--- src/reducers/index.tsx
+++ src/reducers/index.tsx
@@ -1,14 +1,3 @@
-// import graphReducer from './graph.reducer'
-// import graphListReducer from './graph-list.reducer'
-// import { combineReducers } from 'redux'
-
-// const rootReducer = combineReducers({
-//     graphData: graphReducer,
-//     graphList: graphListReducer
-// })
-
-// export default rootReducer
-
 import graphListSlice from './graph-list.reducer'
 import graphSlice from './graph.reducer'
 
