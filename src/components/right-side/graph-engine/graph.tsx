7d45fa9f6b561264b6cadf46bb42442555b1ce17
Andrii Prykhodko <andriiprykhodko@Andriis-Air.home>
2023-11-18 23:36:55 +0000

    graph config

--- src/components/right-side/graph-engine/graph.tsx
+++ src/components/right-side/graph-engine/graph.tsx
@@ -1,8 +1,9 @@
 import { ForceGraph2D } from 'react-force-graph'
-import React, { useState } from 'react'
+import React, { useState, useCallback, useMemo } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import graphFn from '../../../actions/graph-fn.facade'
 import { setGraphData } from '../../../reducers/graph.reducer'
+import {default as graphConfig} from './graph.config.json'
 import { clone } from 'ramda'
 
 const { useRef } = React
@@ -19,6 +20,8 @@ type NodeObject = object & {
   }
 
 interface INodeObjExt {
+    neighbors?: string[]
+    links?: string[]
     color?: string
     __bckgDimensions?: number[]
 }
@@ -29,8 +32,8 @@ const Graph = () => {
     // needed because of Immer (redux Toolkit)
     // data returned is immutable however it should be mutable
     // by design of force graph therefore creation copy is required
-   const mutableGData = clone(gData)
-    
+    const mutableGData = clone(gData)
+
     const dispatch = useDispatch();
 
     const [edge, setEdge] = useState<string[]>([])
@@ -61,36 +64,42 @@ const Graph = () => {
         <ForceGraph2D
             ref={fgRef}
             graphData={mutableGData}
-            d3VelocityDecay={0.3}
+            d3VelocityDecay={graphConfig.d3VelocityDecay}
             nodeAutoColorBy="group"
+            linkWidth={2}
+            linkColor={'white'}
+            linkDirectionalParticles={graphConfig.linkDirectionalParticles}
+            linkDirectionalParticleSpeed={graphConfig.linkDirectionalParticleSpeed}
             
-            nodeCanvasObject={(node: NodeObject & INodeObjExt, ctx, globalScale) => {
+            nodeCanvasObject={(node: INodeObjExt & NodeObject, ctx, globalScale) => {
                 const label = typeof node.id === 'string' ? node.id : '';
-                const fontSize = 9/globalScale;
-                ctx.font = `${fontSize}px Sans-Serif`;
+                const fontSize = graphConfig.fontSize/globalScale;
+                ctx.font = `${fontSize}px ${graphConfig.font}`;
                 const textWidth = label ? ctx.measureText(label).width : 1;
                 const bckgDimensions = [textWidth, fontSize].map(n => n + fontSize * 0.2); // some padding
                 const [w, h] = bckgDimensions
-                ctx.fillStyle = 'rgba(255, 255, 255, 0.1)';
+                ctx.fillStyle = graphConfig.labelBackgroundColor
                 ctx.fillRect(
                     node.x ? node.x - bckgDimensions[0] / 2 : 0,
                     node.y? node.y - bckgDimensions[1] / 2 : 0, 
                     w,
                     h
                 )
-                ctx.textAlign = 'center';
-                ctx.textBaseline = 'middle';
+                ctx.textAlign = graphConfig.textAlign as CanvasTextAlign;
+                ctx.textBaseline = graphConfig.textBaseline as CanvasTextBaseline;
                 ctx.fillStyle = node.color && node.color !== '#a6cee3' ? 
-                    node.color :'rgb(120, 120, 120)';
-                ctx.fillStyle = 'rgb(120, 120, 120)';
+                    node.color : graphConfig.defaultNodeColor;
+                ctx.fillStyle = graphConfig.labelColor
                 ctx.fillText(label, node.x ? node.x : 0, node.y? node.y : 0);
-                
-                // node['__bckgDimensions'] = bckgDimensions; // to re-use in nodePointerAreaPaint
+                node['__bckgDimensions'] = bckgDimensions; // to re-use in nodePointerAreaPaint
             }}
-            // nodePointerAreaPaint={(node, color, ctx) => {
+            
+            // nodePointerAreaPaint={(node: NodeObject & INodeObjExt, color, ctx) => {
             //     ctx.fillStyle = color;
-            //     const bckgDimensions = node.__bckgDimensions;
-            //     bckgDimensions && ctx.fillRect(node.x - bckgDimensions[0] / 2, node.y - bckgDimensions[1] / 2, ...bckgDimensions);
+            //     const bckgDimensions: [number, number] = node.__bckgDimensions as [number, number];
+            //     bckgDimensions && ctx.fillRect(
+            //         (node.x as number) - bckgDimensions[0] / 2, 
+            //         (node.y as number) - bckgDimensions[1] / 2, ...bckgDimensions);
             // }}
 
             // onEngineStop={() => {
