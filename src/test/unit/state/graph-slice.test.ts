ed8b532171fde3cab185df61094dc493fd34d3db
Andrii Prykhodko <andriiprykhodko@Andriis-Air.home>
2023-11-18 15:31:59 +0000

    slice action tests

---
+++ src/test/unit/state/graph-slice.test.ts
@@ -0,0 +1,11 @@
+import {setGraphData} from '../../../reducers/graph.reducer';
+import graphSlice from '../../../reducers/graph.reducer';
+
+
+test('graph slice actions', () => {
+  let state;
+  state = graphSlice({nodes:[{id:'1'},{id:'2'},{id:'3'}, {id:'4'}],links:[{source:'1',target:'2'},{source:'1',target:'3'}]}, setGraphData);
+  expect(state).toEqual({nodes:[{id:'1'},{id:'2'},{id:'3'}, {id:'4'}],links:[{source:'1',target:'2'},{source:'1',target:'3'}]})
+});
+
+
