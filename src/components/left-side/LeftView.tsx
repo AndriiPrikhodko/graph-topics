f52e9789d05597680cc7de4a54938ea33f238007
Andrii Prykhodko <andriiprykhodko@Andriis-Air.home>
2023-11-20 13:59:42 +0000

    add footer

--- src/components/left-side/LeftView.tsx
+++ src/components/left-side/LeftView.tsx
@@ -2,13 +2,15 @@ import './LeftView.css'
 import Header from "./sub-components/header/Header"
 import GraphLoad from "./sub-components/menu/GraphLoad"
 import ActionMenu from './sub-components/menu/ActionMenu'
+import Footer from './sub-components/footer/Footer'
 
 const LeftView: React.FC = () => {
     return (
         <div className="split-small left" data-testid="left-view">
             <Header />
             <ActionMenu />
-            <GraphLoad />  
+            <GraphLoad />
+            <Footer />
         </div>
     )
 }
---
+++ src/components/left-side/sub-components/footer/Footer.tsx
@@ -0,0 +1,18 @@
+import './Footer.css';
+
+const Footer = () => {
+    const currentYear = new Date().getFullYear()
+
+  return (
+    <footer className="footer">
+      <div className="legal-notice">
+        <h2>Author</h2>
+          <p>© {currentYear} Andrii Prykhodko. All Rights Reserved. email:  
+            <a>prykhodkoaa@gmail.com</a>
+          </p>
+        </div>
+    </footer>
+  );
+};
+
+export default Footer
