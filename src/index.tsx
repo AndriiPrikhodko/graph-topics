ac5d01c8a2da40e546578ef7b51490211d244585
Andrii Prykhodko <andriiprykhodko@Andriis-Air.home>
2023-11-20 14:31:08 +0000

    import thunk types

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import { Provider } from 'react-redux'
+import type {} from 'redux-thunk/extend-redux'
 import App from './App'
 import store from './store'
 
