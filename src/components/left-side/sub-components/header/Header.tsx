d47a4450b1fa77cb7d0093f2254b0b771484a8de
Andrii Prykhodko <andriiprykhodko@Andriis-Air.home>
2023-11-21 00:55:01 +0000

    logo and color scheme

--- src/components/left-side/sub-components/header/Header.tsx
+++ src/components/left-side/sub-components/header/Header.tsx
@@ -3,9 +3,12 @@ import './Header.css'
 const Header = () => {
     return (
         <div className="header">
-            <h3 className="title unselectable">
-                <span>Mind</span> Graph
-            </h3>
+            <div className="logo-title">
+                <h3 className="title unselectable">
+                    <span>Mind</span> Graph
+                </h3>
+                <img src={'../../../../graph.svg'} alt="Company Logo" />
+            </div>
             <p>Follow the origins of your ideas.</p>
         </div>
     )
