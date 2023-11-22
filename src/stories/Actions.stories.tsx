d24253c0302c176432aa5b9e368cda0d6d1fd599
Andrii Prykhodko <andriiprykhodko@Andriis-Air.home>
2023-11-22 00:33:00 +0000

    story-book

---
+++ src/stories/Actions.stories.tsx
@@ -0,0 +1,23 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { Provider } from 'react-redux';
+import store from '../store';
+
+import ActionMenu from '../components/left-side/sub-components/menu/ActionMenu';
+
+const meta = {
+  title: 'action menu',
+  component: ActionMenu,
+  decorators: [(Story) => <Provider store={store}><Story/></Provider>],
+  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/react/writing-docs/autodocs
+  tags: ['autodocs'],
+  parameters: {
+    // More on how to position stories at: https://storybook.js.org/docs/react/configure/story-layout
+    layout: 'fullscreen',
+  },
+} satisfies Meta<typeof ActionMenu>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+};
---
+++ src/stories/Header.stories.tsx
@@ -0,0 +1,21 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { Provider } from 'react-redux';
+import store from '../store';
+import LeftView from '../components/left-side/LeftView';
+
+const meta = {
+  title: 'GraphLoad',
+  component: LeftView,
+  decorators: [(Story) => <Provider store={store}><Story/></Provider>],
+  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/react/writing-docs/autodocs
+  tags: ['autodocs'],
+  parameters: {
+    // More on how to position stories at: https://storybook.js.org/docs/react/configure/story-layout
+    layout: 'fullscreen',
+  },
+} satisfies Meta<typeof LeftView>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {};
