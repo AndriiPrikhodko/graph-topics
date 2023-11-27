f9bd3863f00fb4bdb0775b1151e1f45dece71f75
Andrii Prykhodko <andriiprykhodko@Andriis-Air.home>
2023-11-27 19:57:39 +0000

    login form

---
+++ src/components/login-form/index.tsx
@@ -0,0 +1,62 @@
+import './LoginForm.css';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+const LoginForm: React.FC = () => {
+  const navigate = useNavigate();
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit: React.FormEventHandler<HTMLFormElement> = async (event) => {
+    event.preventDefault();
+    const response = await fetch('/api/authenticate', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ username, password }),
+      });
+  
+      if (response.ok) {
+        const { token } = await response.json();
+  
+        // Store the token in local storage or in a cookie
+        localStorage.setItem('token', token);
+  
+        // Redirect to the next page
+        navigate('/graph');
+      } else {
+        // Handle error
+        console.error('Authentication failed');
+      }
+  };
+
+  return (
+  <div className='login-container'>
+    <link href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet' type='text/css'>
+    </link>
+  <div className="login">
+  <h2 className="active"> sign in </h2>
+  <h2 className="nonactive"> sign up </h2>
+  <form onSubmit={handleSubmit}>
+    <input type="text" className="text" name="username"/>
+     <span>username</span>
+    <br/>
+    <br/>
+    <input type="password" className="text" name="password" />
+    <span>password</span>
+    <br/>
+    <input type="checkbox" id="checkbox-1-1" className="custom-checkbox" />
+    <label htmlFor="checkbox-1-1">Keep me Signed in</label>
+    <button className="signin">
+      Sign In
+    </button>
+    <hr/>
+    <a href="#">Forgot Password?</a>
+  </form>
+</div>
+</div>
+)
+}
+
+export default LoginForm
--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,7 +1,6 @@
-import React, { useState } from 'react';
-import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 import { useEffect } from 'react';
+import LoginForm from '../components/login-form';
 // import axios from 'axios';
 
 // const sessionKey = 'your-session-key'; // replace with your session key
@@ -16,45 +15,13 @@ import { useEffect } from 'react';
 
 
 const Login = () => {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
     useEffect(() => {
         navigate('/login');
     }, [navigate]);
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
 
-  const handleSubmit: React.FormEventHandler<HTMLFormElement> = async (event) => {
-    event.preventDefault();
-    const response = await fetch('/api/authenticate', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ username, password }),
-      });
-  
-      if (response.ok) {
-        const { token } = await response.json();
-  
-        // Store the token in local storage or in a cookie
-        localStorage.setItem('token', token);
-  
-        // Redirect to the next page
-        navigate('/graph');
-      } else {
-        // Handle error
-        console.error('Authentication failed');
-      }
-  };
-
-  return (
-    <form onSubmit={handleSubmit}>
-      <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} placeholder="Username" />
-      <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Password" />
-      <button type="submit">Login</button>
-    </form>
-  );
+  return <LoginForm />
 };
 
 export default Login;
