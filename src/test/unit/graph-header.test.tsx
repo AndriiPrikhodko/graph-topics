66fcda51ef50d2d911378bb87f9094cc4ac62657
Andrii Prykhodko <andriiprykhodko@Andriis-Air.home>
2023-11-28 10:31:21 +0000

    unit fix header

--- src/test/unit/graph-header.test.tsx
+++ src/test/unit/graph-header.test.tsx
@@ -3,6 +3,16 @@ import { render } from '@testing-library/react';
 import GraphHeader from '../../components/right-side/GraphHeader';
 import store from '../../store';
 import { Provider } from 'react-redux';
+import { vi } from 'vitest';
+
+vi.mock('react-router-dom', async () => {
+  const router = await vi.importActual<typeof import('react-router-dom')>('react-router-dom');
+  const mockNavigate = vi.fn();
+  return {
+    ...router,
+    useNavigate: vi.fn().mockReturnValue(mockNavigate),
+  };
+});
 
 test('header', () => {
   const { getByTestId } = render(
--- src/test/unit/header.test.tsx
+++ src/test/unit/header.test.tsx
@@ -1,11 +1,27 @@
 import {  test } from 'vitest'
 import { render } from '@testing-library/react';
-// import Graph from '../../components/right-side/graph-engine/graph';
-const Graph = import('../../components/right-side/graph-engine/graph')
+import store from '../../store';
+import { Provider } from 'react-redux';
 import Header from '../../components/left-side/sub-components/header/Header';
+import { vi } from 'vitest';
+
+vi.mock('react-router-dom', async () => {
+  const router = await vi.importActual<typeof import('react-router-dom')>('react-router-dom');
+  const mockNavigate = vi.fn();
+  return {
+    ...router,
+    useNavigate: vi.fn().mockReturnValue(mockNavigate),
+  };
+});
+
+
 
 test('header', () => {
-  const { getByRole } = render(<Header />)
+  const { getByRole } = render(
+    <Provider store={store}>
+      <Header />
+    </Provider>
+  )
   const h3 = getByRole('heading')
   expect(h3).toMatchSnapshot()
 })
