8d33b97ddf041cc3f96683af9c8c87f7d80a7951
Andrii Prykhodko <andriiprykhodko@Andriis-Air.home>
2023-11-30 08:59:40 +0000

    add multiple nodes

--- src/actions/functions/graph/add-edge.ts
+++ src/actions/functions/graph/add-edge.ts
@@ -1,35 +1,54 @@
-
-
-// (this: Edge, graphData :IGraphData & {bind?: (this: Edge) => Edge} )
 export function addEdge(this: string, graphData :IGraphData): IGraphData {
-        const edgeArr = this.split(',')
-        if (edgeArr.length === 2) {
-          const [source, target] = edgeArr
-          const trimmedSource = source.trim()
-          const trimmedTarget = target.trim()
-          let upadateNodes = graphData.nodes
-          const findSource = upadateNodes.find(node => node.id.toLowerCase() === trimmedSource.toLowerCase())
-          const findTarget = upadateNodes.find(node => node.id.toLowerCase() === trimmedTarget.toLowerCase())
-          if(findSource === undefined) upadateNodes.push({
-            id: trimmedSource,
+  const edgeArr = this.split(',')
+  if (edgeArr.length > 1) {
+    const trimmedNodes = edgeArr.map(node => node.trim())
+    
+    let nodeDiff: IGraphData["nodes"] = []
+    let linkDiff: IGraphData["links"] = []
+    let nodeNames: string[] = []
+
+    trimmedNodes
+            .reduce((nodeNames, item) => {
+                const existingNodeIndex = graphData.nodes.findIndex(
+                    node => item.toLowerCase() === node.id.toLowerCase())
+                if(existingNodeIndex > -1) {
+                    nodeNames.push(graphData.nodes[existingNodeIndex].id)
+                }
+                else {
+                    nodeDiff.push({id: item})
+                    nodeNames.push(item)
+                }
+                return nodeNames
+            }, nodeNames)
+
+    linkDiff = nodeNames
+        .reduce((acc, node, index, arr) => {
+        if(node.toLowerCase() !== nodeNames[0].toLowerCase())
+        {
+          acc.push({
+            source: nodeNames[0],
+            target: arr[index]
           })
-          if(findTarget === undefined) upadateNodes.push({
-            id: trimmedTarget,
-          })
-          const updateLinks = [
-            ...graphData.links,
-            {
-              source: findSource?.id ? findSource.id : trimmedSource,
-              target: findTarget?.id ? findTarget.id : trimmedTarget
-            }
-          ]
-          return  {
-              nodes: upadateNodes,
-              links: updateLinks
-            }
         }
-        else {
-          console.log(`incorrect input: ${this}`)
-          return graphData
-        }
-}
+        return acc
+      }, linkDiff)
+
+    const updatedLinks = [
+      ...graphData.links,
+      ...linkDiff
+    ]
+
+    const gNodes = [
+      ...graphData.nodes,
+      ...nodeDiff
+    ]
+    return  {
+        nodes: gNodes,
+        links: updatedLinks
+      }
+  }
+  else {
+    console.log(`incorrect input: ${this}`)
+    return graphData
+  }
+} 
