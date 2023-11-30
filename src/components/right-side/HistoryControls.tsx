12a07f5823f123f3437e2d60bd9e831ebf209279
Andrii Prykhodko <andriiprykhodko@Andriis-Air.home>
2023-11-30 16:00:44 +0000

    fix history tooltips

--- src/components/right-side/HistoryControls.tsx
+++ src/components/right-side/HistoryControls.tsx
@@ -3,8 +3,8 @@ import IconFabrica from '../shared/actions/actionIcon.fabrica'
 const HistoryControls: React.FC = () => {
     return (
       <div className='history-wrapper header-item'>
-        <IconFabrica iconName='undo' label="cancel action" onClick={() => {}}/>
-        <IconFabrica iconName='redo' label="cancel action" onClick={() => {}}/>
+        <IconFabrica iconName='undo' label="undo action" onClick={() => {}}/>
+        <IconFabrica iconName='redo' label="redo action" onClick={() => {}}/>
       </div>
     );
   };
