e8172a04c4187c5cbb2751ecb804180e7b13a264
Andrii Prykhodko <andriiprykhodko@Andriis-Air.home>
2023-12-02 17:41:59 +0000

    close dropdown on click outside

--- src/components/left-side/sub-components/menu/Dropdown.tsx
+++ src/components/left-side/sub-components/menu/Dropdown.tsx
@@ -1,21 +1,39 @@
-import React, { useState } from "react"
+import React, { useState, useRef, useEffect } from "react"
 
 const Dropdown: React.FC<{trigger: JSX.Element & IFuncProps, menu: JSX.Element[]}> = 
     ({ trigger, menu }): JSX.Element => {
     const [open, setOpen] = useState<boolean>(false)
+    let dropdownRef = useRef(null)
 
     const handleOpen = () => {
-        setOpen(!open)
+        setOpen(true)
     }
+
+    const handleClickOutside = (event: MouseEvent) => {
+        const current = dropdownRef.current as null |
+         object & {contains(other: Node | null): boolean}
+        if (current !== null && current.contains(event.target as Node)) {
+            setOpen(true);
+        }
+        else setOpen(false)
+    };
+
+    useEffect(() => {
+        document.addEventListener('mouseup', handleClickOutside);
     
-    return <div className="dropdown">
+        return () => {
+            document.removeEventListener('mouseup', handleClickOutside);
+        };
+    }, [])
+
+    return <div ref={dropdownRef} className="dropdown">
                 {React.cloneElement(trigger, {
                     onClick: handleOpen
                 })}
                 {open?
                 (<ul className="menu" data-testid="dropdown-content">
                     {menu.map((menuItem, index) => (
-                        <li key={index} className = "dropdown-item" data-testid={`dropdown-item-${index}`}>
+                        <li  key={index} className = "dropdown-item" data-testid={`dropdown-item-${index}`}>
                             {React.cloneElement(menuItem, {
                                 onClick: () => {
                                     menuItem.props.onClick(menuItem.props)
