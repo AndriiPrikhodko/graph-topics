bd721923262d314f6404ab1961f334c92b3f7477
Andrii Prykhodko <andriiprykhodko@Andriis-Air.home>
2023-12-03 19:43:15 +0000

    disable on graph function on click outside

--- src/components/right-side/GraphView.tsx
+++ src/components/right-side/GraphView.tsx
@@ -1,10 +1,26 @@
 import Graph from "./graph-engine/graph"
 import GraphHeader from "./GraphHeader"
+import { useDispatch } from 'react-redux'
+import { setEditMode, setGraphFunction } from '../../reducers/common.reducer'
 import './GraphView.css'
 
 const GraphView: React.FC = () => {
+    const dispatch = useDispatch()
+
+    const handleOnBlur:React.FocusEventHandler<HTMLDivElement> = (event) => {
+        console.log('focus')
+        dispatch(setEditMode(false))
+        dispatch(setGraphFunction(null))
+    }
+
     return (
-        <div className="graph-view split-large right" id="graph" data-testid='right-view'>
+        <div 
+            className="graph-view split-large right" 
+            id="graph" 
+            data-testid='right-view'
+            onBlur={handleOnBlur}
+            tabIndex={0}
+            >
             <GraphHeader />
             <Graph />
         </div>
