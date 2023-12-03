5aad362b041d8e6396187da692fcb3a9fcd546a3
Andrii Prykhodko <andriiprykhodko@Andriis-Air.home>
2023-12-03 14:33:08 +0000

    bulk delete graph nodes

--- src/actions/functions/graph/delete-node.ts
+++ src/actions/functions/graph/delete-node.ts
@@ -1,23 +1,42 @@
+/**
+ * 
+ * @param this 
+ * @param graphData 
+ * @returns updatedGraphData with nodes from the input list
+ *  and all their links are removed
+ */
 export const deleteNode = function (this: string, graphData :GraphData): GraphData {
+  const nodeArr = this.split(',')
+  if (nodeArr.length > 1) {
     let isLinked: boolean = true
+    const { nodes, links } = graphData
+    const nodeArrTrimmedLC = nodeArr.map(nodeName => nodeName.trim().toLowerCase())
 
-    const nodeTrimmed = this.trim()
-    const nodes = graphData.nodes
-    const links = graphData.links
+    const filteredLinks = links.filter(link => {
+            isLinked = !(
+              (
+                typeof link.source == 'string' ? 
+                nodeArrTrimmedLC.includes(link.source.toLowerCase()) :
+                nodeArrTrimmedLC.includes(link.source.id.toLowerCase())
+              ) || (
+                typeof link.target == 'string' ? 
+                nodeArrTrimmedLC.includes(link.target.toLowerCase()) :
+                nodeArrTrimmedLC.includes(link.target.id.toLowerCase())
+              )
+            )
+          return isLinked
+        })
+    
     const filteredNodes = nodes
-      .filter(node => node.id !== nodeTrimmed)
-    const filteredLinks = links
-      .filter(link => {
-        if (typeof link.source !== 'string' && typeof link.target !== 'string') {
-          isLinked = !(link.source.id === nodeTrimmed || link.target.id === nodeTrimmed)
-        }
-        else {
-          isLinked = !(link.source === nodeTrimmed || link.target === nodeTrimmed)
-        }
-        return isLinked
-      })
+        .filter(node => !nodeArrTrimmedLC.includes(node.id.toLowerCase()))
+
     return {
         nodes: filteredNodes,
         links: filteredLinks
       }
+    }
+    else {
+      console.log(`incorrect input: ${this}`)
+      return graphData
+    }
   }
