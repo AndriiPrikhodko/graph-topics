8c80355bd56baac9a6122f0cc715d3620aca995e
Andrii Prykhodko <andriiprykhodko@Andriis-Air.home>
2023-12-05 15:51:28 +0000

    local storage helper

---
+++ src/test/unit/helpers/set-local.test.ts
@@ -0,0 +1,19 @@
+import { vi } from 'vitest';
+import { test } from 'vitest';
+import { setGraphDataLocal } from '../../../helpers/local-storage'
+
+globalThis.localStorage = {
+    getItem: vi.fn(),
+    setItem: vi.fn(),
+    clear: vi.fn(),
+    removeItem: vi.fn(),
+    length: 0,
+    key: vi.fn(),
+  }
+
+test('should set graph data to local storage', () => {
+    setGraphDataLocal({nodes: [], links: []})
+    
+    expect(localStorage.setItem).toHaveBeenCalled()
+})
+  
